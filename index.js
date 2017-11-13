64a903994f2446580a1282ed3fb1bfdd9fdbd38b
Josh Hernandez <Josh.Hernandez@norimaconsulting.com>
2017-11-12 19:03:30 -0600

    some things are more promise centric now Also got file download of googleTTS working properly don't have uploading the file to discord for it to actually be spoken yet to work after that, the plan is to remove the file io. I noticed that the stream isn't closing.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const googleTTS = require('google-tts-api');
 const Discord = require('discord.io');
+const request = require('request');
+
 const config = require('./config');
 const utils = require('./utils');
 
@@ -15,18 +18,25 @@ const botUserTag = `<@${config.discord.client.id}>`;
 
 
 function ping(bot,textChannelID) {
-  bot.sendMessage({
-      to: textChannelID,
-      message: "pong"
-  });
+  return new Promise((resolve,reject) => {
+    bot.sendMessage({
+        to: textChannelID,
+        message: "pong"
+    },(error) => {
+      if (error) return reject(error);
+      return resolve();
+    });
+  })
 }
 
 function joinPromise(bot,state,voiceChannelID) {
   return new Promise((resolve, reject) => {
+    // already in the target channel
+    if (state.voice.current === voiceChannelID) return resolve();
+
     bot.joinVoiceChannel( voiceChannelID , (error => {
-      if (error !== null) {
-        return reject(error);
-      }
+      if (error) return reject(error);
+
       state.voice.last = state.voice.current;
       state.voice.current = voiceChannelID;
       return resolve();
@@ -34,14 +44,14 @@ function joinPromise(bot,state,voiceChannelID) {
   });
 }
 
-function join(bot,state,line, errBack) {
+function join(bot,state,line) {
   line = line.substring('join'.length,line.length).trim();
   const voiceChannelID = line;
 
   return joinPromise(bot,state,voiceChannelID);
 }
 
-function say(bot,state,line,errBack) {
+function say(bot,state,line) {
   let languageCode ='en';
   let speed = 1;
   let voiceChannelID = state.voice.current;
@@ -50,9 +60,7 @@ function say(bot,state,line,errBack) {
   [args, leftover] = utils.parseArgs(line);
   const text = leftover.join(' ');
   console.log(`[say] ${line}`)
-  if(text.length === 0) {
-    return errBack(new Error('No text given to say.'));
-  }
+  if(text.length === 0) return Promise.reject(new Error('No text given to say.'));
 
   if(args.speed !== undefined) {
     speed = Number.parseInt(args.speed);
@@ -65,20 +73,46 @@ function say(bot,state,line,errBack) {
   }
 
   if(typeof voiceChannelID !== 'string') {
-    return errBack(new Error('No Voice Channel Set'));
+    return Promise.reject(new Error('No Voice Channel Set'));
   }
 
-  return googleTTS(text, languageCode, speed)   // speed normal = 1 (default), slow = 0.24
-  .then((url) => {
-    return joinPromise(bot,state,voiceChannelID)
-    .then(() => {
-      // say the things
-    })
-  })
-  .catch(function (err) {
-    return errBack(err.stack);
-  });
+  console.log(`[DEBUG][say] ${JSON.stringify(args)}`);
+  console.log(`[DEBUG][say] ${JSON.stringify({text, languageCode, speed})}`);
 
+  return Promise.all([
+    joinPromise(bot,state,voiceChannelID),
+    googleTTS(text, languageCode, speed), // speed normal = 1 (default), slow = 0.24
+  ])
+  .then(([_, url]) => new Promise((resolve, reject) => {
+    if (typeof url !== 'string') return reject('Generated googleTTS url is not type string');
+    if (url.length === 0) return reject('Generated googleTTS url is an empty string');
+    console.log(`[DEBUG][say] url = ${url}`)
+
+    //Then get the audio context
+    bot.getAudioContext(voiceChannelID, (error, stream) => {
+      if (error) return reject(error);
+
+      const tempFile = '/tmp/temp.mp3';
+
+      request
+        .get(url)
+        .on('error', (err) => {
+          if (err) return reject(err);
+        })
+        // .pipe(stream);
+        .pipe(fs.createWriteStream(tempFile))
+        // .pipe(stream,{end: false});
+      //Without {end: false}, it would close up the stream, so make sure to include that.
+
+      fs.createReadStream(tempFile).pipe(stream, {end: false});
+      // fs.createReadStream(tempFile).pipe(stream);
+
+      stream.on('done', resolve);
+      stream.on('error', (error) => {
+        if (error) return reject(error);
+      });
+    });
+  }));
 }
 
 function main() {
@@ -118,25 +152,24 @@ function main() {
 
     // remove the username from the begining of the line
     line = line.substring(botUserTag.length,line.length).trim();
-    if (line.startsWith('ping')) {
-      ping(bot,channelID);
-    } else if (line.startsWith('join')) {
-      join(bot,state,line, (error) => {
-        if (error === null) return;
-        bot.sendMessage({
-            to: channelID,
-            message: `[ERROR] ${error}`,
-        });
+
+    return Promise.resolve()
+    .then(() => {
+      if (line.startsWith('ping')) {
+        return ping(bot,channelID);
+      } else if (line.startsWith('join')) {
+        return join(bot,state,line);
+      } else if (line.startsWith('say')) {
+        return say(bot,state,line);
+      }
+    })
+    .then(() => console.log('DONE'))
+    .catch((error) => {
+      bot.sendMessage({
+          to: channelID,
+          message: `[ERROR] ${error}`,
       });
-    } else if (line.startsWith('say')) {
-      say(bot,state,line, (error) => {
-        if (error === null) return;
-        bot.sendMessage({
-            to: channelID,
-            message: `[ERROR] ${error}`,
-        });
-      });
-    } 
+    })
   });
 
   return bot;
